ea17156e5a0b66ea129865abc29b62a0d8215611
akaw <tvta0195@gmail.com>
2026-09-17 03:46:45 +0700

    ini

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -33,6 +33,7 @@ export const nextJsConfig = [
     rules: {
       ...pluginNext.configs.recommended.rules,
       ...pluginNext.configs['core-web-vitals'].rules,
+      '@next/next/no-html-link-for-pages': 'off',
     },
   },
   {
